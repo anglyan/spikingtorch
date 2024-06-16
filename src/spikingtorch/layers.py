28b874754f228c24185951dda830f86ae2ab567f
anglyan <angel.yanguas@gmail.com>
2024-06-15 21:11:49 -0500

    renamed HardSoft to step

--- src/spikingtorch/layers.py
+++ src/spikingtorch/layers.py
@@ -1,4 +1,4 @@
-# Copyright © 2020-2023, UChicago Argonne, LLC
+# Copyright © 2020-2024, UChicago Argonne, LLC
 # All Rights Reserved
 # Software Name: spikingtorch
 
@@ -6,7 +6,11 @@ import torch
 import torch.nn as nn
 import math as m
 
-class HardSoft(torch.autograd.Function):
+class Step(torch.autograd.Function):
+    """
+    Implements a step function with steep sigmoid gradient propagation 
+
+    """
 
     @staticmethod
     def forward(ctx, input, constant):
@@ -15,7 +19,7 @@ class HardSoft(torch.autograd.Function):
         ctx.constant = constant
         output = torch.sigmoid(constant*input)
         ctx.save_for_backward(output)
-        return H(input)
+        return _H(input)
 
     @staticmethod
     def backward(ctx, grad_output):
@@ -24,20 +28,47 @@ class HardSoft(torch.autograd.Function):
         fakeoutput, = ctx.saved_tensors
         return grad_output * fakeoutput*(1-fakeoutput)*ctx.constant, None
 
-def H(x):
+def _H(x):
+    """Heaviside step function"""
     return 0.5*(torch.sign(x)+1)
 
-hardsoft = HardSoft.apply
+step = Step.apply
+
+class NoisyStep(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, input, constant, noise=None):
+        # ctx is a context object that can be used to stash information
+        # for backward computation
+        ctx.constant = constant
+        output = torch.sigmoid(constant*input)
+        ctx.save_for_backward(output)
+        if noise is None:
+            noise = 0
+        return step(input+noise)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # We return as many input gradients as there were arguments.
+        # Gradients of non-Tensor arguments to forward must be None.
+        fakeoutput, = ctx.saved_tensors
+        return grad_output * fakeoutput*(1-fakeoutput)*ctx.constant, None, None
+
+noisystep = NoisyStep.apply
 
 
-class SpikingLayer(nn.Module):
+class CoarseLIF(nn.Module):
+    """
+    Implements a coarse-scale implementation of a Leaky Integrate and Fire neuron
+
+    """
 
     def __init__(self, tau, v0=1, beta=5):
-        super(SpikingLayer, self).__init__()
+        super(CoarseLIF, self).__init__()
         self.tau = tau
         self.a = m.exp(-1./tau)
         self.b = tau*(1 - self.a)
-        self.c = tau*(1-self.b)
+        self.c = tau*(1 - self.b)
         self.v0 = v0
         self.beta = beta
 
@@ -53,11 +84,19 @@ class SpikingLayer(nn.Module):
             v = self.v
             s = self.s
         self.v = (1-s) * (self.a * v + self.b * xi) + s * self.c * xi
-        self.s = hardsoft(self.v-self.v0, self.beta)
+        self.s = step(self.v-self.v0, self.beta)
         return self.s
 
 
+class SpikingLayer(CoarseLIF):
+    pass
+
+
 class LIF(nn.Module):
+    """
+    Implements a Leaky Integrate and Fire neuron
+
+    """
 
     def __init__(self, tau=8, v0=1, beta=5, decay=None):
         super(LIF, self).__init__()
@@ -80,12 +119,15 @@ class LIF(nn.Module):
         if init:
             self.reset(xi)    
         self.v = (1-self.s) * self.a * self.v + self.b * xi
-        self.s = hardsoft(self.v-self.v0, self.beta)
+        self.s = step(self.v-self.v0, self.beta)
         return self.s
 
 
 class IF(nn.Module):
+    """
+    Implements an Integrate and Fire neuron
 
+    """
     def __init__(self, v0=1, beta=5):
         super(IF, self).__init__()
         self.v0 = v0
@@ -99,7 +141,7 @@ class IF(nn.Module):
         if init:
             self.reset(xi)
         self.v = (1-self.s) * self.v +  xi
-        self.s = hardsoft(self.v-self.v0, self.beta)
+        self.s = step(self.v-self.v0, self.beta)
         return self.s
 
 
@@ -111,7 +153,7 @@ class McPitts(nn.Module):
         self.beta = beta
 
     def forward(self, x):
-        return hardsoft(x-self.thr, self.beta)
+        return step(x-self.thr, self.beta)
 
 
 class SpikingDecay(nn.Module):
@@ -131,7 +173,7 @@ class SpikingDecay(nn.Module):
             v = self.v
             s = self.s
         self.v = (1-s) * self.a * v + xi
-        self.s = hardsoft(self.v-self.v0, self.beta)
+        self.s = step(self.v-self.v0, self.beta)
         return s
 
 
@@ -147,6 +189,6 @@ class SpikingLayer2(nn.Module):
 
     def forward(self, xi, s, v):
         v = (1-s) * (self.a * v + self.b * xi)
-        s = hardsoft(v-self.v0, self.beta)
+        s = step(v-self.v0, self.beta)
         return s, v
 
